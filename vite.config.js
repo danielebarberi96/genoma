f32955bc7cc629abb71136f6df8a910d1f5c4415
Daniele Barberi <daniele.barberi@polimi.it>
2025-09-26 15:05:42 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import nodePolyfills from 'rollup-plugin-node-polyfills'
 
 export default defineConfig({
+  base: '/genoma/',
   plugins: [react()],
   build: {
     rollupOptions: {
